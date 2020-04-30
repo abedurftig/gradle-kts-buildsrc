e7add809bc7fc86349bb546855f34be07fb8f21a
abedurftig <dasnervtdoch@googlemail.com>
2020-04-30 10:26:47 +0200

    Create test class for application module

--- files/root/new-module.sh
+++ files/root/new-module.sh
@@ -46,9 +46,28 @@ object Application {
 }" > "$MODULE_NAME"/src/main/kotlin/$PACKAGE_DIR/Application.kt
 }
 
-#function create_application_test () {
-#
-#}
+function create_application_test () {
+    echo "package $ROOT_PACKAGE
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.test.annotation.MicronautTest
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import javax.inject.Inject
+
+@MicronautTest
+class ApplicationTest {
+
+    @Inject
+    private lateinit var applicationContext: ApplicationContext
+
+    @Test
+    fun sanityTest() {
+        Assertions.assertThat(applicationContext.environment.activeNames).hasSize(1)
+        Assertions.assertThat(applicationContext.environment.activeNames).contains(\"test\")
+    }
+}" > "$MODULE_NAME"/src/test/kotlin/$PACKAGE_DIR/ApplicationTest.kt
+}
 
 function create_application () {
     echo "Creating application"
